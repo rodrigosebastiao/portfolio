b8c5bcdbca69f85558f07d57426d445c2c61d9ee
rodrigosebastiao <rodrigodanielss@gmail.com>
2023-11-11 05:59:07 -0300

    sanity.cli.ts test

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,4 +1,4 @@
-import {defineCliConfig} from 'sanity/cli'
+/* import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
@@ -6,3 +6,19 @@ export default defineCliConfig({
     dataset: 'production'
   }
 })
+ */
+
+
+import { loadEnvConfig } from '@next/env'
+import { defineCliConfig } from 'sanity/cli'
+
+const dev = process.env.NODE_ENV !== 'production'
+loadEnvConfig(__dirname, dev, { info: () => null, error: console.error })
+
+// @TODO report top-level await bug
+// Using a dynamic import here as `loadEnvConfig` needs to run before this file is loaded
+// const { projectId, dataset } = await import('lib/sanity.api')
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+
+export default defineCliConfig({ api: { projectId, dataset } })
